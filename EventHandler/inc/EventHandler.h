f9516c69777c61aa223078b9e8b2c8939b886b74
Vasily Davydov <vasily.davydov@metropolia.fi>
2022-09-21 10:39:29 +0300

    eventhandler: init #6

---
+++ EventHandler/inc/EventHandler.h
@@ -0,0 +1,17 @@
+/*
+ * EventHandler.h
+ *
+ *  Created on: Sep 21, 2022
+ *      Author: tylen
+ */
+
+#ifndef EVENTHANDLER_H_
+#define EVENTHANDLER_H_
+
+class EventHandler {
+public:
+	EventHandler();
+	virtual ~EventHandler();
+};
+
+#endif /* EVENTHANDLER_H_ */
---
+++ EventHandler/src/EventHandler.cpp
@@ -0,0 +1,18 @@
+/*
+ * EventHandler.cpp
+ *
+ *  Created on: Sep 21, 2022
+ *      Author: tylen
+ */
+
+#include <EventHandler.h>
+
+EventHandler::EventHandler() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EventHandler::~EventHandler() {
+	// TODO Auto-generated destructor stub
+}
+
