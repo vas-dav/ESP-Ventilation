5eb79336762f63d387e774103903033833a09d59
Vasily Davydov <vasilydavydovmail@yahoo.com>
2022-10-05 10:27:12 +0300

    state-handler: #6 rename the class

---
+++ StateHandler/src/StateHandler.cpp
@@ -0,0 +1,27 @@
+/*
+ * StateHandler.cpp
+ *
+ *  Created on: Sep 21, 2022
+ *      Author: tylen
+ */
+
+#include <StateHandler.h>
+
+StateHandler::StateHandler(EVENT_HANDL * btns) {
+	// TODO Auto-generated constructor stub
+
+}
+
+StateHandler::~StateHandler() {
+	// TODO Auto-generated destructor stub
+}
+
+int StateHandler::getSetPresuure(){
+	//TODO
+	return 0;
+}
+
+int StateHandler::getSetSpeed(){
+	// TODO
+	return 0;
+}
