f95c15e13c2a6d587634222f83058ca9c2e4b2f3
Vasily Davydov <vasilydavydovmail@yahoo.com>
2022-10-05 11:47:14 +0300

    state-handler: #6 change constructor args

--- StateHandler/src/StateHandler.cpp
+++ StateHandler/src/StateHandler.cpp
@@ -7,46 +7,57 @@
 
 #include <StateHandler.h>
 
-StateHandler::StateHandler(LiquidCrystal * lcd) {
-	this->_lcd = lcd;
-	// TODO
+StateHandler::StateHandler (LiquidCrystal *lcd)
+{
+  this->_lcd = lcd;
+  // TODO
 }
 
-StateHandler::~StateHandler() {
-	// TODO Auto-generated destructor stub
+StateHandler::~StateHandler ()
+{
+  // TODO Auto-generated destructor stub
 }
 
-unsigned int StateHandler::getSetPressure(){
-	return (unsigned int)this->set[PRESSURE].getCurrent();
+unsigned int
+StateHandler::getSetPressure ()
+{
+  return (unsigned int)this->set[PRESSURE].getCurrent ();
 }
 
-unsigned int StateHandler::getSetSpeed(){
-	return (unsigned int)this->set[FAN_SPEED].getCurrent();
+unsigned int
+StateHandler::getSetSpeed ()
+{
+  return (unsigned int)this->set[FAN_SPEED].getCurrent ();
 }
 
-void StateHandler::displaySet(unsigned int value1, unsigned int value2){
-	// TODO
-	/** 
-	 * MANUAL MODE:
-	 * ----------------
-	 * SPEED: 20%
-	 * PRESSURE: XXPa
-	 * ----------------
-	 * AUTO MODE:
-	 * ----------------
-	 * PRESSURE SET: 35Pa
-	 * PRESSURE CUR: XXPa
-	 * ----------------
-	 */
+void
+StateHandler::displaySet (unsigned int value1, unsigned int value2)
+{
+  // TODO
+  /**
+   * MANUAL MODE:
+   * ----------------
+   * SPEED: 20%
+   * PRESSURE: XXPa
+   * ----------------
+   * AUTO MODE:
+   * ----------------
+   * PRESSURE SET: 35Pa
+   * PRESSURE CUR: XXPa
+   * ----------------
+   */
 }
 
-void StateHandler::HandleState(const Event &event){
-	(this->*current)(event);
+void
+StateHandler::HandleState (const Event &event)
+{
+  (this->*current) (event);
 }
 
-void StateHandler::SetState(state_pointer newstate){
-	(this->*current)(Event(Event::eExit));
-	current = newstate;
-	(this->*current)(Event(Event::eEnter));
-
+void
+StateHandler::SetState (state_pointer newstate)
+{
+  (this->*current) (Event (Event::eExit));
+  current = newstate;
+  (this->*current) (Event (Event::eEnter));
 }
