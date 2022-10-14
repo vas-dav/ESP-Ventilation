1b4c37b9c500d5a0424b2e75dfc364578a820ac5
Vasily Davydov <vasilydavydovmail@yahoo.com>
2022-10-14 13:58:30 +0300

    state-handler: counter: modify limits

--- StateHandler/src/Counter.cpp
+++ StateHandler/src/Counter.cpp
@@ -7,39 +7,49 @@
 
 #include "Counter.h"
 
-void Counter::inc() {
-    if(init >= up_lim){
-        init = 0;
-    } else{
-        ++init;
+void
+Counter::inc ()
+{
+  if (init < up_lim)
+    {
+      ++init;
     }
 }
 
-void Counter::dec() {
-    if(init <= 0){
-        init = up_lim;
-    } else{
-        --init;
+void
+Counter::dec ()
+{
+  if (init - 1 > 0)
+    {
+      --init;
     }
 }
 
-
-int Counter::getCurrent(){
+int
+Counter::getCurrent ()
+{
   return this->init;
 }
 
-Counter::Counter(int i, int up) {
-    up_lim = up;
-    if(i > up){
-        init = up;
-    }else if(i < 0){
-        init = 0;
-    }else{
-        init = i;
+Counter::Counter (int i, int up)
+{
+  up_lim = up;
+  if (i > up)
+    {
+      init = up;
     }
-
+  else if (i < 0)
+    {
+      init = 0;
+    }
+  else
+    {
+      init = i;
+    }
 }
 
-void Counter::setInit(int i){
+void
+Counter::setInit (int i)
+{
   init = i;
 }
