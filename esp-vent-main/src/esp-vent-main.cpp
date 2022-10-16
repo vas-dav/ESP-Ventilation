dd00c13714b68b0a7b63571260b70c572c832fb0
Vasily Davydov <vasilydavydovmail@yahoo.com>
2022-10-16 12:46:36 +0300

    main: display blank values

--- esp-vent-main/src/esp-vent-main.cpp
+++ esp-vent-main/src/esp-vent-main.cpp
@@ -17,6 +17,7 @@
 #endif
 
 #include "DigitalIoPin.h"
+#include "LiquidCrystal.h"
 #include "StateHandler.h"
 
 #include <cr_section_macros.h>
@@ -40,34 +41,54 @@ main (void)
   Board_LED_Set (0, true);
 #endif
 #endif
+  /** Lcd & stateHandler */
+  Chip_RIT_Init (LPC_RITIMER);
+  DigitalIoPin rs (0, 29, false, true, false);
+  DigitalIoPin en (0, 9, false, true, false);
+  DigitalIoPin d4 (0, 10, false, true, false);
+  DigitalIoPin d5 (0, 16, false, true, false);
+  DigitalIoPin d6 (1, 3, false, true, false);
+  DigitalIoPin d7 (0, 0, false, true, false);
+  LiquidCrystal lcd (&rs, &en, &d4, &d5, &d6, &d7);
+  StateHandler ventMachine (&lcd);
+  //
+  lcd.setCursor (0, 0);
+  lcd.print ("Test");
 
-  DigitalIoPin b_up ();
-  DigitalIoPin b_down ();
-  DigitalIoPin b_toggle ();
-  bool b_up_state = false, b_down_state = false, b_toggle_state = false;
-
-  StateHandler ventMachine;
+  /** Common pins */
+  DigitalIoPin b_up (0, 7, true, true, true); // A5
+  bool b_up_state = false;
+  DigitalIoPin b_down (0, 6, true, true, true); // A4
+  bool b_down_state = false;
+  DigitalIoPin b_toggle (0, 5, true, true, true); // A3
+  bool b_toggle_state = false;
 
   int16_t pressure = 1;
 
   while (1)
     {
       if (b_up.read ())
-        b_up_state = true;
+        {
+          b_up_state = true;
+        }
       if (!b_up.read () && b_up_state)
         {
           ventMachine.HandleState (Event (Event::eKey, BUTTON_CONTROL_UP));
           b_up_state = false;
         }
       if (b_down.read ())
-        b_down_state = true;
+        {
+          b_down_state = true;
+        }
       if (!b_down.read () && b_down_state)
         {
           ventMachine.HandleState (Event (Event::eKey, BUTTON_CONTROL_DOWN));
           b_down_state = false;
         }
       if (b_toggle.read ())
-        b_toggle_state = true;
+        {
+          b_toggle_state = true;
+        }
       if (!b_toggle.read () && b_toggle_state)
         {
           ventMachine.HandleState (
