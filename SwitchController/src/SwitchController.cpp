c0c85df36f78e3a34b471512bca59412f078242f
Vasily Davydov <vasilydavydovmail@yahoo.com>
2022-10-17 08:55:48 +0300

    switch-controller: init empty class
    This class is meant for controlling switch behaviour, such as
    button hold etc

---
+++ SwitchController/src/SwitchController.cpp
@@ -0,0 +1,24 @@
+/*
+ * SwitchController.cpp
+ *
+ *  Created on: Oct 17, 2022
+ *      Author: tylen
+ */
+
+#include <SwitchController.h>
+
+SwitchController::SwitchController (DigitalIoPin *button, Timer *timer,
+                                    StateHandler *handler)
+{
+  // TODO Auto-generated constructor stub
+}
+
+SwitchController::~SwitchController ()
+{
+  // TODO Auto-generated destructor stub
+}
+
+void
+SwitchController::listen ()
+{
+}
